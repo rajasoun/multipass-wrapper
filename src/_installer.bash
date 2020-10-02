f1eebe3f24796aba1daf08b420dfcb6d6342e6ed
Raja. Soundaramourty <rajasoun@cisco.com>
2020-10-02 12:47:12 +0530

    Automated Kali Setup

--- src/_installer.bash
+++ src/_installer.bash
@@ -4,5 +4,5 @@ function install_kali_linux(){
     VM_NAME=$1
     multipass exec  "$VM_NAME" -- git clone https://github.com/s-h-3-l-l/katoolin3
     multipass exec  "$VM_NAME" -- chmod +x  katoolin3/install.sh
-    multipass exec  "$VM_NAME" -- sudo katoolin3/install.sh
+    multipass exec  "$VM_NAME" -- bash -c "cd katoolin3 && sudo ./install.sh"
 }
